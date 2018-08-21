9f7f102e2008148b280ab3628b5447ee002368b5
Maxim Malov <mmalov@hwdtech.ru>
2018-08-21 20:40:01 +0600

    Fix another stream reference

--- stories/connections.js
+++ stories/connections.js
@@ -1,13 +1,17 @@
-import { HubConnectionBuilder } from '@aspnet/signalr';
+import { HubConnectionBuilder, HttpTransportType } from '@aspnet/signalr';
 import createConnectionContext from '../lib/createConnectionContext';
 
 const urlNamespace = 'https://signalr-sample.herokuapp.com/';
 
 const createValidConnection = () =>
-  new HubConnectionBuilder().withUrl(`${urlNamespace}counter`).build();
+  new HubConnectionBuilder()
+    .withUrl(`${urlNamespace}counter`, HttpTransportType.WebSockets)
+    .build();
 
 const createInvalidConnection = () =>
-  new HubConnectionBuilder().withUrl(`${urlNamespace}counter1`).build();
+  new HubConnectionBuilder()
+    .withUrl(`${urlNamespace}counter1`, HttpTransportType.WebSockets)
+    .build();
 
 export const CounterConnectionContext = createConnectionContext(
   createValidConnection
