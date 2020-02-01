eba320645ff2c4e84f239ac2e157ab7b5de62b66
Maxim Malov <m.malov.v@gmail.com>
2020-02-02 01:18:25 +0600

    wipe

--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export {createConnectionContext} from "./createConnectionContext";
--- test-support/defer.ts
+++
@@ -1,7 +0,0 @@
-export function deferResolve<T>(ms: number, value?: T) {
-  return new Promise(resolve => setTimeout(() => resolve(value), ms));
-}
-
-export function deferReject(ms: number, reason: Error) {
-  return new Promise((resolve, reject) => setTimeout(() => reject(reason), ms));
-}
